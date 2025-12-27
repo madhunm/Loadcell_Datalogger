095e144658633e3a6fca3f52ddab94c919ef333e
Madhu Nuggehalli <madhu.nuggehalli@gmail.com>
2025-12-27 14:20:37 +0530

    Remove all contents from main branch

--- include/gpio.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <Arduino.h>
-
-// Initialize all GPIO pins to their correct directions and states.
-// This should be called once during setup() before initializing peripherals.
-void gpioInit();
-
-
